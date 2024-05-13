e39134a3476332e5648f9cd0a3461c1b070df052
w4t3rcs <uimanovmaks@gmail.com>
2024-05-13 23:18:25 +0200

    Added Spring Boot Admin and added .env file

--- src/main/java/com/cryptodvala/exchange/ExchangeServiceApplication.java
+++ src/main/java/com/cryptodvala/exchange/ExchangeServiceApplication.java
@@ -1,5 +1,6 @@
 package com.cryptodvala.exchange;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @EnableAsync
 @EnableScheduling
+@EnableAdminServer
 @SpringBootApplication
 public class ExchangeServiceApplication {
     public static void main(String[] args) {
