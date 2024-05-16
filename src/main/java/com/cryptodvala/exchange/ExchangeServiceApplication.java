706c830aa73a2c43eca0ada5188cbd438c961332
w4t3rcs <uimanovmaks@gmail.com>
2024-05-16 11:38:45 +0200

    Some small changes in application.yaml

--- src/main/java/com/cryptodvala/exchange/ExchangeServiceApplication.java
+++ src/main/java/com/cryptodvala/exchange/ExchangeServiceApplication.java
@@ -1,15 +1,9 @@
 package com.cryptodvala.exchange;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisConfiguration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -26,19 +20,5 @@ public class ExchangeServiceApplication {
     @Bean
     public WebClient webClient() {
         return WebClient.create();
-    }
-
-    @Bean
-    public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory(RedisConfiguration redisConfiguration) {
-        return new LettuceConnectionFactory(redisConfiguration, LettuceClientConfiguration.builder().useSsl().build());
-    }
-
-    @Bean
-    public RedisConfiguration redisConfiguration(@Value("${REDIS_HOST}") String redisHost, @Value("${REDIS_PORT}") int redisPort, @Value("${REDIS_PASSWORD}") String redisPassword) {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(redisHost);
-        redisStandaloneConfiguration.setPort(redisPort);
-        redisStandaloneConfiguration.setPassword(redisPassword);
-        return redisStandaloneConfiguration;
     }
 }
--- src/main/java/com/cryptodvala/exchange/repository/ExchangeRepository.java
+++ src/main/java/com/cryptodvala/exchange/repository/ExchangeRepository.java
@@ -1,9 +1,9 @@
 package com.cryptodvala.exchange.repository;
 
 import com.cryptodvala.exchange.entity.Exchange;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import reactor.core.publisher.Flux;
 
-public interface ExchangeRepository extends ReactiveCrudRepository<Exchange, String> {
+public interface ExchangeRepository extends R2dbcRepository<Exchange, String> {
     Flux<Exchange> findAllByOrderByMarketPriceDesc();
 }
