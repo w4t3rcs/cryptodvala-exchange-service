970328a290c152a62ccc53d123feab2970cde811
w4t3rcs <uimanovmaks@gmail.com>
2024-05-24 23:18:20 +0200

    Refactor Binance API URL and add error handling in BinanceDataLoader
    The URL to the Binance API has been refactored to a constant for better manageability. Moreover, error handling has been enhanced in the BinanceDataLoader by logging any errors encountered during data loading and returning an empty Flux instead of throwing the exception further up the call stack.

--- src/main/java/com/cryptodvala/exchange/async/impl/BinanceDataLoader.java
+++ src/main/java/com/cryptodvala/exchange/async/impl/BinanceDataLoader.java
@@ -9,11 +9,13 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class BinanceDataLoader implements DataLoader {
+    private static final String BINANCE_API_URL = "https://fapi.binance.com/fapi/v1/premiumIndex";
     private final ExchangeService exchangeService;
     private final WebClient webClient;
 
@@ -22,10 +24,13 @@ public class BinanceDataLoader implements DataLoader {
     @Scheduled(fixedDelay = 2500)
     public void load() {
         webClient.get()
-                .uri("https://fapi.binance.com/fapi/v1/premiumIndex")
+                .uri(BINANCE_API_URL)
                 .retrieve()
                 .bodyToFlux(ExchangeDto.class)
-                .cache()
+                .onErrorResume(e -> {
+                    log.error("Error while loading data from Binance", e);
+                    return Flux.empty();
+                }).cache()
                 .subscribe(exchangeService::saveExchange);
     }
 }
